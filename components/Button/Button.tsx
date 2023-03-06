c737bbb9d053d76d5be053ca2d080aa58698a341
Danylo Diachenko <danildiachenko23@gmail.com>
2023-03-06 10:55:36 +0200

    htag, button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import cn from "classnames";
+import { ButtonProps } from "./Button.props";
+import styles from "./Button.module.css";
+
+export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+    return (
+        <button
+            className={cn(styles.button, {
+                [styles.primary]: appearance == "primary",
+                [styles.ghost]: appearance == "ghost",
+            })}
+        >
+            {children}
+        </button>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { Htag } from "../components";
+import { Button } from "../components";
+
 export default function Home(): JSX.Element {
-    return <div>віфвфі</div>;
+    return (
+        <>
+            <Htag tag="h1">Текст</Htag>
+            <Htag tag="h2">Текст</Htag>
+            <Htag tag="h3">Текст</Htag>
+
+            <Button appearance="primary">Кнопка</Button>
+            <Button appearance="ghost">Кнопка</Button>
+        </>
+    );
 }
