229ad3113bfb351951748a90da51eaf357898370
Danylo Diachenko <danildiachenko23@gmail.com>
2023-03-09 09:44:38 +0200

    footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,5 +1,20 @@
+import cn from "classnames";
+import { format } from "date-fns";
 import { FooterProps } from "./Footer.props";
+import styles from "./Footer.module.css";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-    return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+    return (
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                OwlTop © 2022 - {format(new Date(), "yyyy")} Все права защищены
+            </div>
+            <a href="#" target="_blank">
+                Пользовательское соглашение
+            </a>{" "}
+            <a href="#" target="_blank">
+                Политика конфиденциальности
+            </a>
+        </footer>
+    );
 };
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,20 +1,18 @@
+import { FunctionComponent } from "react";
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { LayoutProps } from "./Layout.props";
-import styles from "./layoutProps.module.css";
-import { FunctionComponent, ReactNode } from "react";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>{children}</div>
-            </div>
-            <Footer />
-        </>
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.body}>{children}</div>
+            <Footer className={styles.footer} />
+        </div>
     );
 };
 
