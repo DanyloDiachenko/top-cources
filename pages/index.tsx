89b508fa8b7ec1fe89d34059cc42bd2dda5e7aa1
Danylo Diachenko <danildiachenko23@gmail.com>
2023-04-05 22:17:40 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,16 @@
-import { GetStaticProps } from "next";
-import React, { useState } from "react";
-import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 
-import { Button, Htag, Input, P, Rating, Tag, Textarea } from "../components";
 import { withLayout } from "../layout/Layout";
-import { MenuItem } from "../interfaces/menu.interface";
-import { API } from "../helpers/api";
 
 const Home = (): JSX.Element => {
-    const [rating, setRating] = useState<number>(4);
+    const router = useRouter();
 
-    return (
-        <>
-            <Htag tag="h1">Заголовок</Htag>
-            <Button appearance="primary" arrow="right">
-                Кнопка
-            </Button>
-            <Button appearance="ghost" arrow="down">
-                Кнопка
-            </Button>
-            <P size="l">Большой</P>
-            <P>Средний</P>
-            <P size="s">Маленький</P>
-            <Tag size="s">Ghost</Tag>
-            <Tag size="m" color="red">
-                Red
-            </Tag>
-            <Tag size="s" color="green">
-                Green
-            </Tag>
-            <Tag color="primary">Green</Tag>
-            <Rating rating={rating} isEditable setRating={setRating} />
-            <Input placeholder="тест" />
-            <Textarea placeholder="тест area" />
-        </>
-    );
+    useEffect(() => {
+        router.push("/cources");
+    }, []);
+
+    return <></>;
 };
 
 export default withLayout(Home);
-
-export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-    const firstCategory = 0;
-    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
-        firstCategory,
-    });
-    return {
-        props: {
-            menu,
-            firstCategory,
-        },
-    };
-};
-
-interface HomeProps extends Record<string, unknown> {
-    menu: MenuItem[];
-    firstCategory: number;
-}
